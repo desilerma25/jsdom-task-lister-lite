2aacda2c14274ace7b95bd4c95b6fb347c279b11
Desiree Lerma <desi.lerma96@yahoo.com>
2021-01-30 17:33:01 -0600

    save

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.querySelector("#create-task-form");
+  const taskList = document.querySelector("#tasks");
+  taskForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const newTask = document.querySelector("#new-task-description").value;
+    taskList.innerHTML += `<li> ${newTask}
+    <button data-action = "delete"> X </button>
+    </li>`;
+    taskList.addEventListener("click", function (event) {
+      if (event.target.dataset.action === "delete") {
+        event.target.parentElement.remove();
+      }
+    });
+    taskForm.reset();
 });
+});
+
+// type a task into input field (form)
+//click submit button
+//task provided should appear on the DOM after submit clicked
+//prevent POST from happening after click submit
